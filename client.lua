ae57eb2afdd3666f693f128996c49f419382c5a3
SpMex <113365369+yungmexx@users.noreply.github.com>
2023-06-23 22:59:07 -0700

    Improvements and added hidetext
    Didnt realize I didnt include a hide text event lol. Was testing only with commands.

--- client.lua
+++ client.lua
@@ -15,43 +15,49 @@ function DrawAdvancedText(x, y, sc, text, r, g, b, a, font, jus)
     DrawText(x, y - 0.005)
 end
 
-RegisterNetEvent('v-dialogtext')
-AddEventHandler('v-dialogtext', function(text)
-    IsTextVisible = true
-    while IsTextVisible do
-        Wait(0)
-
-        splittext1 = string.sub(text, 0, 98)
-        splittext2 = string.sub(text, 99)
-
-        -- Draw UI elements
-        DrawRect(0.5, 0.92, 0.40, 0.07, 0, 0, 0, 150)
-        DrawRect(0.5, 0.965, 0.40, 0.0025, 180, 180, 180, 150)
-
-        DrawAdvancedText(0.5, 0.91, 0.4, tostring(splittext1), 255, 255, 255, CurrentTextOpacity, 6, 0)
-        DrawAdvancedText(0.5, 0.93, 0.4, tostring(splittext2), 255, 255, 255, CurrentTextOpacity, 6, 0)
-    end
-end)
-
-function dialogtext(text, time)
-    if IsTextVisible then
-        IsTextVisible = false -- Stop displaying current text
-        Wait(100) -- Wait for a brief moment before showing the new text
-    end
-    IsTextVisible = true
-    TriggerEvent('v-dialogtext', text) -- Trigger the event with the new text
+function DrawUIElements()
+    DrawRect(0.5, 0.92, 0.40, 0.07, 0, 0, 0, 150)
+    DrawRect(0.5, 0.965, 0.40, 0.0025, 180, 180, 180, 150)
 end
 
-function hidetext()
+function HideText()
     IsTextVisible = false
 end
 
--- Example usage
-RegisterCommand('dialogtext', function(source, args, rawCommand, time)
-    local text = table.concat(args, " ")
-    dialogtext(text, time)
+function ShowText(text)
+    if IsTextVisible then
+        Wait(100) -- Wait for a brief moment before showing the new text
+    end
+    IsTextVisible = true
+    TriggerEvent('v-dialogue:showtext', text) -- Trigger the event with the new text
+end
+
+RegisterNetEvent('v-dialogue:showtext', function(text)
+    IsTextVisible = true
+    local splittext1 = string.sub(text, 0, 98)
+    local splittext2 = string.sub(text, 99)
+
+    -- Draw UI elements
+    DrawUIElements()
+
+    DrawAdvancedText(0.5, 0.91, 0.4, tostring(splittext1), 255, 255, 255, CurrentTextOpacity, 6, 0)
+    DrawAdvancedText(0.5, 0.93, 0.4, tostring(splittext2), 255, 255, 255, CurrentTextOpacity, 6, 0)
 end)
 
-RegisterCommand('hidetext', function()
-    hidetext()
+RegisterNetEvent('v-hidetext', HideText)
+
+-- Example usage
+--[[
+RegisterCommand('dialogtext', function(source, args, rawCommand)
+    local text = table.concat(args, " ")
+    ShowText(text)
 end)
+
+RegisterCommand('hidetext', HideText)
+
+RegisterCommand('testest', function()
+    TriggerEvent('v-dialogue:showtext', "This is an Example")
+    Wait(1000)
+    TriggerEvent('v-hidetext')
+end)
+--]]
